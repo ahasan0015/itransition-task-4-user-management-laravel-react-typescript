2c0e5d418e26d72167f3e376dd039d6496020851
ahasan0015 <ahasanstu@gmail.com>
2026-06-19 01:03:27 +0600

    Revert "add user controller index method, user model a hasapitoken, and api sanctum route declair"
    This reverts commit bf89f61367febb98383d080fd9eaa5195d11ca0c.

--- frontend/src/config/axios.tsx
+++ frontend/src/config/axios.tsx
@@ -1,11 +1,7 @@
-import axios from "axios";
+import axios from 'axios';
 
-const api = axios.create({ baseURL: "http://localhost:8000/api" });
-
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) config.headers.Authorization = `Bearer ${token}`;
-  return config;
+const api = axios.create({
+    baseURL: 'http://localhost:8000/api', 
 });
 
 export default api;
