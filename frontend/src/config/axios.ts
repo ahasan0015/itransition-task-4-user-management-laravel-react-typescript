7c2d40a0bd06323a4ce6c6588a60e3e2a47876d2
ahasan0015 <ahasanstu@gmail.com>
2026-06-21 19:10:16 +0600

    add live url in congig/axios.ts

--- frontend/src/config/axios.ts
+++ frontend/src/config/axios.ts
@@ -1,7 +1,11 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api",
+  //for local development
+  // baseURL: "http://localhost:8000/api",
+  
+  //for production
+  baseURL: "https://lcm.ahasanhabibroxy.online/api",
   headers: {
     "Accept": "application/json",
     "Content-Type": "application/json",
